6bada1f85c9eb180b3f35fec26d06be4edce4bc3
pjtsearch <piwithewiwi@gmail.com>
2017-10-23 09:01:01 -0700

    Update anscroll.js

--- anscroll.js
+++ anscroll.js
@@ -1,26 +1,17 @@
-$("body").prepend("<style> *{ margin:0; padding:0;}#topScroll{ transform: rotate(180deg); position:absolute; top:0; left:0; width:100%; height:20px;}#bottomScroll{ position:fixed; bottom:0; left:0; width:100%; height:20px;} </style><img style='display:none' src='androidscroll.png' id='topScroll'><img style='display:none' src='androidscroll.png' id='bottomScroll'>");
-$(window).load(function(){
- $(window).scroll(function() {   
-   if($(window).scrollTop() + $(window).height() == $(document).height()) {
-       //alert("bottom!");
-       //alert("top!");
-       //$("#img").fadeIn();
-       $('#bottomScroll').animate({height:'32px', opacity:'1'}, 500);
-       $('#bottomScroll').animate({height:'20px', opacity:'0'}, 700);
-
-   // Fade Out
-   //$("#img").fadeOut();
-   }else{
-     $("#img").hide();
+$("body").prepend("<style> *{ margin:0; padding:0;}#topScroll{ transform: rotate(180deg); position:absolute; top:0; left:0; width:100%; height:20px;}#bottomScroll{ position:fixed; bottom:0; left:0; width:100%; height:20px;} </style><img style='opacity:0' src='androidscroll.png' id='topScroll'><img style='opacity:0' src='androidscroll.png' id='bottomScroll'>");
+$(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height()-$(window).height()){
+        $('#bottomScroll').animate({height:'32px', opacity:'1'});
+       $('#bottomScroll').animate({height:'20px', opacity:'0'});
+    }else{
+     $("#bottomScroll").css("opacity", "0");
    }
 });
  $(window).scroll(function() {   
    if($(window).scrollTop() <= 0) {
-       //alert("top!");
    $('#topScroll').animate({height:'32px', opacity:'1'}, 500);
        $('#topScroll').animate({height:'20px', opacity:'0'}, 700);
-       //$("#myImage").fadeOut();
    }
 });
 
-});
+
